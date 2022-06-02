52cad48d24e478acb641f1002312ae7310a3b13a
jfernando <jfhernan@ualberta.ca>
2022-06-02 17:09:18 -0600

    fixed: wrong path to cifar data

--- src/definitions.py
+++ src/definitions.py
@@ -1,5 +1,5 @@
 import os
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
-MNIST_DATA_PATH = os.path.join(ROOT, "function_approximators", "problems", "supervised_learning", "mnist")
-CIFAR_DATA_PATH = os.path.join(ROOT, "function_approximators", "problems", "supervised_learning", "cifar")
+MNIST_DATA_PATH = os.path.join(ROOT, "problems", "supervised_learning", "mnist")
+CIFAR_DATA_PATH = os.path.join(ROOT, "problems", "supervised_learning", "cifar")
