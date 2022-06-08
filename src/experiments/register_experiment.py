f9967a2653ce523db2e99e18bfecec1f16823f9b
jfernando <jfhernan@ualberta.ca>
2022-06-08 13:07:32 -0600

    changed: underscores for dashes

--- src/experiments/register_experiment.py
+++ src/experiments/register_experiment.py
@@ -54,7 +54,7 @@ def register_experiment(experiment_name: str, experiment_path: str, experiment_c
 def main():
     arguments = argparse.ArgumentParser()
     arguments.add_argument("--experiment-name", action="store", type=str, required=True)
-    arguments.add_argument("--experiment-class_name", action="store", type=str, required=False)
+    arguments.add_argument("--experiment-class-name", action="store", type=str, required=False)
     arguments.add_argument("--experiment-path", action="store", type=str, required=True)
     parsed_args = arguments.parse_args()
 
