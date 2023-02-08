5fa1cab0e33afa466473a87877fd41226ba68463
Jfernando4 <jfhernan@ualberta.ca>
2023-02-08 14:25:21 -0700

    added: mnist slurm example

---
+++ examples/mnist_example/README.md
@@ -0,0 +1,69 @@
+# Running experiments in slurm
+
+The intention of this example is to show how to schedule experiments with slurm. 
+For this project, all the tests were performed in the Beluga server of the Digital Research Alliance of Canada, 
+formerly known as Compute Canada.
+
+The python script `mnist_example.py` is simply a classification task using MNIST. 
+The parameters of the experiment are specified in `config.json`. 
+Given the different parameters, it would take a long time to run it serially in a local computer.
+Thus, this is the kind of experiment that would be best run in parallel using slurm. 
+
+## Config files
+The experiment slurm config file contains this information:
+
+    {
+      "job_name": "mnist",
+      "output_dir": "/scratch/jfernan/example/outputs",
+      "output_filename": "mnist-%n-%j.out",
+      "time": "4:00:00",
+      "mem": "8G",
+      "mail-type": "BEGIN,END,FAIL",
+      "mail-user":"jfhernan@ualberta.ca",
+      "cpus-per-task": 4,
+      "max_runs_per_job": 10,
+      "main_dir": "/home/jfernan/Research_Project_Manager",
+      "account": "def-sutton",
+      "gpus-per-node": "0"
+    }
+
+Let's go through all the options used in the config file:
+* `job_name`: this is the name of the job. You can see the progress of the job by using this name as reference. 
+Nevertheless, the name doesn't have to be unique since slurm creates a unique ID for each new job. 
+* `output_dir`: path to directory where slurm can dump output files. These output files will contain anything printed 
+by the script or any error messages. The symbols `%n` and `%j` are replaced by the node and job IDs, respectively. 
+Thus, the name `mnist-%n-%j.out` will result in a separate output file for each different job scheduled in slurm.
+* `time`: how long to run the experiment for. Make sure to give it enough time or to handle timeouts in such a way that
+you don't lose any data.
+* `mem`: amount of memory to use. 
+* `mail-type`: types of mail to receive from slurm regarding the job status.
+* `mail-user`: who to send emails to.
+* `cpus-per-task`: number of cpus to use for each slurm job.
+* `max_runs_per_job`: maximum number of runs to try to run in a single slurm job. All of these runs should be able to
+finish running before hitting the maximum amount of time allowed.
+* `main_dir`: path to the main directory of the experiment. This not necessarily the same as the path to where the 
+experiment file is located, but the path to where the source code of the experiment and the `venv` are located.
+* `account`: whose slurm account to use.
+* `gpus-per-node`: how many gpus to use for each different node.
+
+These are all the slurm options supported by `mlproj_manager=0.0.2`.
+
+## How to set up and run the experiment
+First, while in the `Research_Project_Manager` directory run these two lines:
+
+    export PYTHONPATH=.
+    python3 ./mlproj_manager/experiments/register_experiment.py \
+        --experiment-name mnist_example --experiment-path ./examples/mnist_example/mnist_example.py \
+        --experiment-class-name MNISTExperiment
+
+This will register the experiment so that `mlproj_manager` can run it by using an experiment wrapper.
+Next, schedule your jobs using the following command:
+
+    python3 ./mlproj_manager/main.py \
+        --experiment-name mnist_example --experiment-config-path ./examples/mnist_example/config.json \
+        --use-slurm --slurm-config-path ./examples/mnist_example/slurm_config.json --verbose
+
+You should see the output of running the command `sbatch` in slurm, which is simply a number corresponding to
+the ID of the job that was just schedule. You'll see several of these depending on the number of jobs that were
+scheduled.
+
---
+++ examples/mnist_example/mnist_example.py
@@ -0,0 +1,190 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import numpy as np
+import os
+import time
+# from project files
+from mlproj_manager.definitions import ROOT, MNIST_DATA_PATH
+from mlproj_manager.experiments import Experiment
+from mlproj_manager.function_approximators import DeepNet
+from mlproj_manager.util import turn_off_debugging_processes, get_random_seeds, access_dict
+from mlproj_manager.util.neural_networks import layer, init_weights_kaiming, get_optimizer
+from mlproj_manager.problems import MnistDataSet
+os.chdir(ROOT)
+
+
+class MNISTExperiment(Experiment):
+
+    def __init__(self, exp_params: dict, results_dir: str, run_index: int, verbose=True):
+        super().__init__(exp_params, results_dir, run_index, verbose)
+
+        # set debugging options for pytorch
+        debug = access_dict(exp_params, key="debug", default=True, val_type=bool)
+        turn_off_debugging_processes(debug)
+
+        # define torch device
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        """ For reproducibility """
+        random_seeds = get_random_seeds()
+
+        """ Experiment parameters """
+        self.optimizer = exp_params["optimizer"]
+        self.stepsize = exp_params["stepsize"]
+        self.training_epochs = exp_params["training_epochs"]
+        self.batch_size = exp_params['batch_size']
+        self.num_units_per_layer = access_dict(exp_params, key="num_units_per_layer", default="large", val_type=int)
+        self.num_layers = access_dict(exp_params, key="num_layers", default="large", val_type=int)
+        self.gate_function = access_dict(exp_params, key="gate_function", default="relu",
+                                         choices=["relu", "sigmoid", "tanh", None])
+        self.data_path = exp_params['data_path']
+
+        """ Training constants """
+        self.num_classes = 10
+        self.image_dims = (28, 28)
+        self.num_images_per_epoch = 60000
+
+        """ Network set up """
+        # initialize network architecture
+        architecture = []
+        for _ in range(self.num_layers):
+            architecture.append(layer(type="linear", parameters=(None, self.num_units_per_layer), gate="relu"))
+        architecture.append(layer(type="linear", parameters=(None, self.num_classes), gate=None))
+        # initialize network
+        self.net = DeepNet(architecture, image_dims=self.image_dims)
+        # initialize network weights
+        init_func = lambda m: init_weights_kaiming(m, nonlinearity=self.gate_function, normal=True)
+        self.net.apply(init_func)
+        # initialize optimizer
+        self.optim = get_optimizer(self.optimizer, self.net.parameters(), stepsize=self.stepsize)
+        # define loss function
+        self.loss = torch.nn.CrossEntropyLoss(reduction="mean")
+        # move network to device
+        self.net.to(self.device)
+
+        """ For summaries and reproducibility """
+        self.random_seed = random_seeds[self.run_index]
+        # summaries
+        for data_type in ["test", "avg_train"]:
+            for summary_type in ["acc", "loss"]:
+                name = data_type + "_" + summary_type + "_per_epoch"
+                self.results_dict[name] = torch.zeros(self.training_epochs, device=self.device, dtype=torch.float32)
+
+    # -------------------- For manipulating data -------------------- #
+    def get_data(self, train=True, return_data_loader=False):
+        """ Loads MNIST data set """
+        mnist_data = MnistDataSet(root_dir=self.data_path,
+                                  train=train,
+                                  device=self.device,
+                                  image_normalization="max",
+                                  label_preprocessing="one-hot",
+                                  use_torch=True)
+
+        if return_data_loader:
+            dataloader = DataLoader(mnist_data, batch_size=self.batch_size, shuffle=True, num_workers=0,
+                                    pin_memory=False)
+            return mnist_data, dataloader
+
+        return mnist_data
+
+    # -------------------- For storing summaries -------------------- #
+    def store_summaries(self, avg_train_loss: torch.Tensor, test_loss: torch.Tensor, avg_train_acc: torch.Tensor,
+                        test_acc: torch.Tensor, idx: int = 0):
+
+        self.results_dict["test_acc_per_epoch"][idx] += test_acc
+        self.results_dict["test_loss_per_epoch"][idx] += test_loss
+        self.results_dict["avg_train_acc_per_epoch"][idx] += avg_train_acc
+        self.results_dict["avg_train_loss_per_epoch"][idx] += avg_train_loss
+
+    # -------------------- For running the experiment -------------------- #
+    def train_one_epoch(self, mnist_dataloader: DataLoader):
+
+        loss_per_batch = torch.zeros(len(mnist_dataloader), dtype=torch.float32, device=self.device)
+        acc_per_batch = torch.zeros(len(mnist_dataloader), dtype=torch.float32, device=self.device)
+        for i, sample in enumerate(mnist_dataloader):
+            image = sample["image"].reshape(self.batch_size, np.prod(self.image_dims))
+            label = sample["label"]
+
+            for param in self.net.parameters(): param.grad = None   # apparently faster than optim.zero_grad()
+
+            outputs = self.net.forward(image, return_activations=False)
+            current_loss = self.loss(outputs, label)
+            current_loss.backward()
+            self.optim.step()
+
+            loss_per_batch[i] += current_loss.detach()
+            accuracy = torch.mean((outputs.argmax(axis=1) == label.argmax(axis=1)).to(torch.float32))
+            acc_per_batch[i] += accuracy.detach()
+
+        return loss_per_batch, acc_per_batch
+
+    def evaluate_network(self, test_data: MnistDataSet):
+        with torch.no_grad():
+            test_outputs = self.net.forward(test_data[:]["image"].reshape(-1, np.prod(self.image_dims)),
+                                            return_activations=False)
+            test_labels = test_data[:]["label"]
+
+            loss = self.loss(test_outputs, test_labels)
+            acc = torch.mean((test_outputs.argmax(axis=1) == test_labels.argmax(axis=1)).to(torch.float32))
+
+        return loss, acc
+
+    def train(self, mnist_data_loader: DataLoader, test_data: MnistDataSet):
+        self._print("Training network...")
+
+        for e in range(self.training_epochs):
+            self._print("\tEpoch number: {0}".format(e + 1))
+            loss, acc = self.train_one_epoch(mnist_dataloader=mnist_data_loader)
+            test_loss, test_accuracy = self.evaluate_network(test_data)
+
+            self.store_summaries(avg_train_loss=torch.mean(loss),
+                                 avg_train_acc=torch.mean(acc),
+                                 test_loss=test_loss,
+                                 test_acc=test_accuracy,
+                                 idx=e)
+
+            self._print("\t\tTest accuracy: {0:.4f}".format(test_accuracy))
+
+    def run(self):
+
+        # load data
+        training_data, training_dataloader = self.get_data(train=True, return_data_loader=True)
+        test_data = self.get_data(train=False, return_data_loader=False)
+
+        # train network
+        self.train(mnist_data_loader=training_dataloader, test_data=test_data)
+
+        # save summaries to memory
+        self.store_results()
+
+
+def main():
+    """
+    This is a quick demonstration of how to run the experiments. For a more systematic run, use the mlproj_manager
+    scheduler.
+    """
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    experiment_parameters = {
+        "optimizer": "sgd",
+        "stepsize": 0.1,
+        "training_epochs": 10,
+        "batch_size": 32,
+        "num_units_per_layer": 100,
+        "num_layers": 1,
+        "gate_function": "relu",
+        "data_path": MNIST_DATA_PATH,
+    }
+
+    initial_time = time.perf_counter()
+    exp = MNISTExperiment(experiment_parameters,
+                          results_dir=os.path.join(file_path, "results"),
+                          run_index=0,
+                          verbose=True)
+    exp.run()
+    final_time = time.perf_counter()
+    print("The running time in minutes is: {0:.2f}".format((final_time - initial_time) / 60))
+
+
+if __name__ == "__main__":
+    main()
