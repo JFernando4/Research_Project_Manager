09deb9b1157c0e22138330c3e10960714db769f3
Jfernando4 <jfhernan@ualberta.ca>
2023-06-12 11:02:35 -0600

    added: indicator for plotting results

--- mlproj_manager/experiments/abstract_experiment.py
+++ mlproj_manager/experiments/abstract_experiment.py
@@ -5,7 +5,8 @@ from mlproj_manager.file_management.file_and_directory_management import save_ex
 class Experiment:
 
     """ Abstract class for experiments that outlines all the methods and experiment is expected to have """
-    def __init__(self, exp_params: dict, results_dir: str, run_index: int, verbose=True):
+    def __init__(self, exp_params: dict, results_dir: str, run_index: int, verbose: bool = True,
+                 plot_results: bool = False):
         """
         Initialize the experiment
         :param exp_params: (dict) all the information necessary to run the experiment
@@ -16,6 +17,7 @@ class Experiment:
         self.results_dir = results_dir
         self.run_index = run_index
         self.verbose = verbose
+        self.plot_results = plot_results
         save_experiment_config_file(results_dir, exp_params, run_index)
 
         self.results_dict = {}
